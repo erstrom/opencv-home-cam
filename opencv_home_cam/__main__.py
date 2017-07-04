64aba25ab80e69a9d4e5216b309e60930478f768
Erik Stromdahl <erik.stromdahl@gmail.com>
2017-07-04 16:17:08 +0200

    add logging option
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

--- opencv_home_cam/__main__.py
+++ opencv_home_cam/__main__.py
@@ -4,6 +4,8 @@ import sys
 import os
 import cv2
 import signal
+import logging
+import logging.config
 from opencv_home_cam import HomeCamManager, HomeCamException
 
 description = "OpenCV home cam test app"
@@ -27,6 +29,11 @@ def load_options():
                              help="Cascade file(s) for OpenCV.")
     parser.add_argument('-c', '--config-file',
                              help="Configuraton file.")
+    parser.add_argument('-l', '--log-config-file',
+                             help="Logging configuraton file. This is the "
+                                  "configuration for the Python logging "
+                                  "facility. See the Python documentation "
+                                  "for more details.")
 
     parsed_args = parser.parse_args()
 
@@ -47,6 +54,17 @@ def main():
             sys.stderr.write('Missing input config file\n')
             exit(1)
 
+        if parsed_args.log_config_file:
+            logging.config.fileConfig(parsed_args.log_config_file)
+        else:
+            sys.stderr.write("Missing logger configuration.\n"
+                             "Default configuration will be used.\n")
+            logging.basicConfig(stream=sys.stderr,
+                                level=logging.DEBUG,
+                                format='%(asctime)s %(message)s')
+
+        logger = logging.getLogger(__name__)
+
         signal.signal(signal.SIGINT, signal_handler)
         signal.signal(signal.SIGTERM, signal_handler)
 
@@ -54,7 +72,9 @@ def main():
                              cascade_files=parsed_args.cascade_file)
 
         hcm.start()
+        logger.info("Waiting for HomeCamManager to finish\n")
         hcm.wait()
+        logger.info("HomeCamManager finished\n")
 
     except IOError as err:
         sys.stderr.write('{}\n'.format(err))
