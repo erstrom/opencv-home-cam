e6a5406d79fafa2cf3960579d8a0261a11577b60
Erik Stromdahl <erik.stromdahl@gmail.com>
2017-07-27 10:49:51 +0200

    add example action script
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

---
+++ example-actions/print-env.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#
+# Example action script for opencv-home-cam.
+# All env vars set up by opencv-home-cam are printed.
+
+echo "Action timestamp raw: $TIME_STAMP_RAW"
+echo "Action timestamp: $TIME_STAMP_DATE"
+echo "Action cascade: $CASCADE"
+echo "Action trigger: $TRIGGER"
