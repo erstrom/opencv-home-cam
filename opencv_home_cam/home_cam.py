0076d293871dfb455c5b88dfe950abc43d78f507
Erik Stromdahl <erik.stromdahl@gmail.com>
2017-08-08 10:03:35 +0200

    home_cam.py: remove python version detection
    This detection code is redundant and not used.
    Besides, we are expected to run Python3 always (see README.rst)
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

--- opencv_home_cam/home_cam.py
+++ opencv_home_cam/home_cam.py
@@ -7,20 +7,6 @@ import logging
 from collections import namedtuple
 from .detector import Detector
 from .recorder import Recorder
-try:
-    unicode = unicode
-except NameError:
-    # 'unicode' is undefined, must be Python 3
-    str = str
-    unicode = str
-    bytes = bytes
-    basestring = (str, bytes)
-else:
-    # 'unicode' exists, must be Python 2
-    str = str
-    unicode = unicode
-    bytes = str
-    basestring = basestring
 
 
 # frame          - The current frame (image)
