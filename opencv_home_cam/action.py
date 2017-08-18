a41b432cf7f12ffb0c345fea219a947aaec7e47c
Erik Stromdahl <erik.stromdahl@gmail.com>
2017-08-18 23:41:22 +0200

    action.py: code cleanup
    Changed code structure somewhat in order to reduce the
    indentation level.
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

--- opencv_home_cam/action.py
+++ opencv_home_cam/action.py
@@ -35,33 +35,36 @@ class Action:
 
     def invoke(self, detection, detector_name, frame):
 
-        if ((self._trigger_detection and detection) or
-            (self._trigger_no_detection and not detection)):
-            detection_time = time.time()
-            if (self._last_detection_time + self._cool_down_time) > detection_time:
-                self._logger.info("Action skipped due to cool-down")
-                return
-            self._last_detection_time  = detection_time
-            for action_detector in self._detectors:
-                if action_detector == detector_name:
-                    if self._save_frame:
-                        # Create a temporary file for the current frame
-                        image = tempfile.NamedTemporaryFile(suffix='.jpg',
-                                                            prefix='opencv-home-cam-',
-                                                            dir=self._save_frame_dir)
-                        image_path = image.name
-                        cv2.imwrite(image_path, frame)
-                    else:
-                        image_path = "No image"
-                    # It is time to invoke the action script
-                    self._invoke_command(detection,
-                                         detector_name,
-                                         self._command,
-                                         image_path)
-                    if self._save_frame:
-                        # Remove the temporary file.
-                        image.close()
-                    break
+        if ((not self._trigger_detection and detection) or
+            (not self._trigger_no_detection and not detection)):
+            return
+
+        detection_time = time.time()
+        if (self._last_detection_time + self._cool_down_time) > detection_time:
+            self._logger.info("Action skipped due to cool-down")
+            return
+
+        self._last_detection_time  = detection_time
+        for action_detector in self._detectors:
+            if action_detector == detector_name:
+                if self._save_frame:
+                    # Create a temporary file for the current frame
+                    image = tempfile.NamedTemporaryFile(suffix='.jpg',
+                                                        prefix='opencv-home-cam-',
+                                                        dir=self._save_frame_dir)
+                    image_path = image.name
+                    cv2.imwrite(image_path, frame)
+                else:
+                    image_path = "No image"
+                # It is time to invoke the action script
+                self._invoke_command(detection,
+                                     detector_name,
+                                     self._command,
+                                     image_path)
+                if self._save_frame:
+                    # Remove the temporary file.
+                    image.close()
+                break
 
     def _invoke_command(self, detection, detector_name, command, image_path):
 
